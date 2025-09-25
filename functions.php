c4d71ee6c72e765f5857adf8548082af772e0de0
kazdotng <105362471+kazdotng@users.noreply.github.com>
2025-09-25 22:55:40 +0100

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+// This CSS hides the post/page title in Astra theme
+// Targeted selectors:
+// .ast-single-post .entry-title  → single post titles
+// .page .entry-title             → page titles
+// 'display: none;' removes them from view
+.ast-single-post .entry-title,
+    .page .entry-title {
+        display: none;
+    }
+?>
